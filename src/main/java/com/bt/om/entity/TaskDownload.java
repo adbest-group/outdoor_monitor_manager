fb8cea1333846ea82f84083b6c3958cff16dba92
hfrbox <873735999@qq.com>
2018-06-22 16:45:42 +0800

    忘记提交了（黄芙蓉）

---
+++ src/main/java/com/bt/om/entity/TaskDownload.java
@@ -0,0 +1,27 @@
+package com.bt.om.entity;
+
+public class TaskDownload {
+
+	private String key;
+	private Integer value;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+//	public String getValue() {
+//		return value;
+//	}
+//	public void setValue(String value) {
+//		this.value = value;
+//	}
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	
+	
+}

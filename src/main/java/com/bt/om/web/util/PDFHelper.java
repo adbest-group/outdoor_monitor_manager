e6e971d3f29de499338eb4c908076dd32bd8e2a0
guomw <516985724@qq.com>
2018-08-23 11:38:43 +0800

    整理PDF导出代码

---
+++ src/main/java/com/bt/om/web/util/PDFHelper.java
@@ -0,0 +1,470 @@
+package com.bt.om.web.util;
+
+import com.adtime.common.lang.StringUtil;
+import com.alibaba.druid.util.StringUtils;
+import com.bt.om.entity.AdApp;
+import com.bt.om.entity.AdMonitorTaskFeedback;
+import com.bt.om.service.IAdMonitorTaskService;
+import com.bt.om.util.ConfigUtil;
+import com.bt.om.util.MapUtil;
+import com.bt.om.util.pdf.AlternatingBackground;
+import com.bt.om.web.controller.ExcelController;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * pdf 帮助类
+ * PDF 导出代码重新封装
+ *
+ * @author guomw
+ * @date 2018-08-22
+ */
+@Service
+public class PDFHelper {
+    private static final Logger logger = Logger.getLogger(ExcelController.class);
+
+    private String file_upload_path = ConfigUtil.getString("file.upload.path");
+    private static BaseFont bfChinese = null;
+    private static BaseFont secfont = null;
+
+    private static Font fontChinese = null;
+    private String real_path = "";
+    private static final String SEC_FONT = "/static/font/simhei.ttf";//   SIMKAI.TTF
+    private static Rectangle pageSize = null;
+    private Document document = null;
+
+    private static final String GROUP_LOGO_PATH = "/static/images/grouplogo.png";
+    private static final String JF_LOGO_PATH = "/static/images/jflogo.png";
+    private static final String GONG_ZHANG_PATH = "/static/images/gongzhang.png";
+
+    private PdfWriter writer;
+    private PdfContentByte cb;
+
+    /**
+     * 生成报表
+     *
+     * @param request
+     * @param path
+     * @param ids
+     * @param activityAdseatIds
+     * @param taskIds
+     * @param map
+     * @param adapp
+     * @param title
+     * @return boolean
+     */
+    public boolean buildReport(HttpServletRequest request,
+                               List<AdMonitorTaskFeedback> taskFeedbacks,
+                               String path,
+                               List<Integer> ids,
+                               List<Integer> activityAdseatIds,
+                               Map<Integer, Integer> taskIds,
+                               Map<Integer, List<String>> map,
+                               AdApp adapp,
+                               String title) {
+
+        try {
+            if (ids != null && ids.size() > 0) {
+                real_path = request.getSession().getServletContext().getRealPath("/");
+                if(bfChinese==null) {
+                    //bfChinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+                    bfChinese=BaseFont.createFont(real_path + SEC_FONT, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                }
+                // 创建字体，设置family，size，style,还可以设置color
+                if(fontChinese==null) {
+                    fontChinese = new Font(bfChinese, 20, Font.NORMAL);
+                }
+                if(pageSize==null) {
+                    pageSize = new Rectangle(1920, 1080);
+                }
+
+                if(secfont==null) {
+                    secfont = BaseFont.createFont(real_path + SEC_FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                }
+                document = new Document(pageSize);
+                writer = PdfWriter.getInstance(document, new FileOutputStream(path));
+                document.open();
+                for (Integer monitorTaskId : ids) {
+                    //生成第一页内容
+                    buildFirstPageContent(taskIds, map, monitorTaskId, adapp, title);
+                    //广告位信息
+                    List<String> adList = map.get(activityAdseatIds.get(ids.indexOf(monitorTaskId)));
+                    //生成第二页内容
+                    buildTwoPageContent(adList, monitorTaskId, taskFeedbacks, adapp);
+                }
+                document.close();
+                return true;
+            }
+        } catch (Exception e) {
+            logger.error("批量导出pdf失败",e);
+            e.printStackTrace();
+            return false;
+        }
+
+
+        return false;
+    }
+
+    /**
+     * 生成第一页内容
+     *
+     * @param taskIds
+     * @param map
+     * @param monitorTaskId
+     * @param adapp
+     * @param title
+     * @throws DocumentException
+     * @throws IOException
+     * @author guomw
+     */
+    private void buildFirstPageContent(Map<Integer, Integer> taskIds,
+                                       Map<Integer, List<String>> map,
+                                       Integer monitorTaskId,
+                                       AdApp adapp,
+                                       String title) throws DocumentException, IOException {
+        document.setMargins(122f, 100f, 300f, 50f);
+        document.newPage();
+        cb = writer.getDirectContent();
+        cb.beginText();
+        //设置标题位置及字体大小
+        cb.setFontAndSize(secfont, 53);
+        cb.showTextAligned(PdfContentByte.ALIGN_LEFT, title, 120, 860, 0);
+        cb.endText();
+
+        Integer backId = taskIds.get(monitorTaskId);
+        List<String> data = map.get(backId);
+        //创建第一页表格
+        PdfPTable firstPageTable = createFirstPageTable(data);
+        document.add(firstPageTable);
+        //插入logo
+        insertLogo(adapp);
+    }
+
+
+    /**
+     * 生成第二页内容
+     *
+     * @param adList
+     * @param monitorTaskId
+     * @param taskFeedbacks
+     * @param adapp
+     * @throws Exception
+     */
+    private void buildTwoPageContent(List<String> adList,
+                                     Integer monitorTaskId,
+                                     List<AdMonitorTaskFeedback> taskFeedbacks,
+                                     AdApp adapp) throws Exception {
+
+        //生成广告位图片信息页, 每个广告位一页
+        document.setMargins(-1020f, 100f, 300f, 50f);
+        document.newPage();
+        PdfPTable table = createTwoPageTable(adList);
+        document.add(table);
+
+        //广告位信息生成
+        cb = writer.getDirectContent();
+        cb.beginText();
+        //设置字体和大小
+        cb.setFontAndSize(secfont, 53);
+        cb.showTextAligned(PdfContentByte.ALIGN_LEFT, "广告位信息 ", 120, 860, 0);
+        cb.endText();
+
+        for (AdMonitorTaskFeedback feedback : taskFeedbacks) {
+            if (feedback.getMonitorTaskId().equals(monitorTaskId)) {
+                insertTwoPageAdImage(adList, feedback);
+                break;
+            }
+        }
+        //插入logo
+        insertLogo(adapp);
+        //插入公章
+        Image gongZhangLogo = Image.getInstance(real_path + GONG_ZHANG_PATH);
+        gongZhangLogo.setAlignment(Image.ALIGN_CENTER);
+        //控制图片大小
+        gongZhangLogo.scaleAbsolute(250, 180);
+        //控制图片位置
+        gongZhangLogo.setAbsolutePosition(1150, 5);
+        document.add(gongZhangLogo);
+    }
+
+
+    /**
+     * 创建第一页表格
+     *
+     * @param list
+     * @return
+     * @throws DocumentException
+     */
+    private PdfPTable createFirstPageTable(List<String> list) throws DocumentException {
+        PdfPTable table = new PdfPTable(4);
+
+        table.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+        //1100f
+        table.setTotalWidth(1620f);
+        table.setLockedWidth(true);
+        table.setHorizontalAlignment(50);
+        table.setSpacingAfter(20.0f);
+        table.setSpacingBefore(20.0f);
+        table.setWidths(new int[]{1, 1, 1, 1});
+
+        PdfPCell cell = new PdfPCell(new Paragraph("点位名称", fontChinese));
+        cell.setBackgroundColor(new BaseColor(211, 211, 211));
+        cell.setBorder(0);
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setMinimumHeight(50f);
+        table.addCell(cell);
+//		cell = new PdfPCell(new Paragraph("媒体主", fontChinese));
+        cell = new PdfPCell(new Paragraph("城市", fontChinese));
+        cell.setBackgroundColor(new BaseColor(211, 211, 211));
+        cell.setBorder(0);
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cell);
+//		cell = new PdfPCell(new Paragraph("广告位编号", fontChinese));
+        cell = new PdfPCell(new Paragraph("上刊日期", fontChinese));
+        cell.setBackgroundColor(new BaseColor(211, 211, 211));
+        cell.setBorder(0);
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cell);
+        cell = new PdfPCell(new Paragraph("广告位编号", fontChinese));
+//		cell = new PdfPCell(new Paragraph("投放日期", fontChinese));
+        cell.setBackgroundColor(new BaseColor(211, 211, 211));
+        cell.setBorder(0);
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cell);
+        //点位名称 现在取的是 详细位置
+        cell = new PdfPCell(new Paragraph(list.get(5), fontChinese));
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setMinimumHeight(30f);
+        cell.setBorder(0);
+        table.addCell(cell);
+        //媒体主
+        //cell = new PdfPCell(new Paragraph(list.get(20), fontChinese));
+        //城市
+        cell = new PdfPCell(new Paragraph(list.get(3), fontChinese));
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorder(0);
+        table.addCell(cell);
+        //投放日期
+        //cell = new PdfPCell(new Paragraph(list.get(7)+" - "+list.get(8), fontChinese));
+        // 报告日期
+        cell = new PdfPCell(new Paragraph(list.get(26), fontChinese));
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorder(0);
+        table.addCell(cell);
+        //广告位编号
+        cell = new PdfPCell(new Paragraph(list.get(6), fontChinese));
+        cell.setHorizontalAlignment(1);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorder(0);
+        table.addCell(cell);
+        return table;
+    }
+
+    /**
+     * 创建第二页表格
+     *
+     * @param list
+     * @return
+     * @throws Exception
+     */
+    private PdfPTable createTwoPageTable(List<String> list) throws Exception {
+        PdfPTable table = new PdfPTable(2);
+        table.getDefaultCell().setBorder(PdfPCell.NO_BORDER);
+        table.setTotalWidth(550f);
+        table.setLockedWidth(true);
+        table.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+        table.setSpacingAfter(20.0f);
+        table.setSpacingBefore(20.0f);
+        table.setWidths(new int[]{1, 1});
+        table.getDefaultCell().setMinimumHeight(60f);
+        table.addCell(getCell("品牌",fontChinese));
+        table.addCell(getCell(list.get(27), fontChinese));
+        table.addCell(getCell("媒体类型", fontChinese));
+        table.addCell(getCell(list.get(18), fontChinese));
+        table.addCell(getCell("广告位编号", fontChinese));
+        table.addCell(getCell(list.get(6), fontChinese));
+        table.addCell(getCell("地址(位置)", fontChinese));
+        StringBuffer location = new StringBuffer();
+        //省
+        if (StringUtil.isNotBlank(list.get(2))) {
+            location.append(list.get(2));
+        }
+        //市
+        if (StringUtil.isNotBlank(list.get(3))) {
+            location.append(list.get(3));
+        }
+        //主要路段
+        if (StringUtil.isNotBlank(list.get(4))) {
+            location.append(list.get(4));
+        }
+        //详细位置
+        if (StringUtil.isNotBlank(list.get(5))) {
+            location.append(list.get(5));
+        }
+//      table.addCell(new Paragraph(location.toString(), fontChinese));//具体位置
+        //主要路段
+        table.addCell(getCell(list.get(4), fontChinese));
+        //起止日期
+        table.addCell(getCell("发布期", fontChinese));
+        table.addCell(getCell(list.get(7).replaceAll("-",".") + "-" + list.get(8).replaceAll("-","."), fontChinese));
+
+//            table.addCell(new Paragraph(list.get(7), fontChinese));//开始监测时间
+//            table.addCell(new Paragraph(list.get(8), fontChinese));//结束监测时间
+
+        //加入隔行换色事件
+        PdfPTableEvent event = new AlternatingBackground();
+        table.setTableEvent(event);
+        table.addCell(table.getDefaultCell());
+
+        String url = "http://api.map.baidu.com/staticimage?width=540&height=300&center=";
+        String lon;
+        String lat;
+        if (list.get(13) != null && list.get(14) != null) {
+            //广告位经度
+            lon = list.get(13);
+            //广告位纬度
+            lat = list.get(14);
+        } else {
+            //省经度
+            lon = list.get(2);
+            //市纬度
+            lat = list.get(3);
+        }
+        String urlString = url + lon + "," + lat + "&markers=" + lon + "," + lat + "&zoom=11";
+        String filename = lon + lat + ".jpg";
+        MapUtil.download(urlString, filename, file_upload_path);
+        return table;
+    }
+
+    private PdfPCell getCell(String text,Font font){
+        PdfPCell cell= new PdfPCell(new Paragraph(text, font));
+        cell.setUseAscender(true);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setMinimumHeight(50f);
+        return  cell;
+    }
+
+
+    /**
+     * 插入第二页广告图片
+     *
+     * @param list
+     * @param feedback
+     * @throws DocumentException
+     * @throws IOException
+     */
+    private void insertTwoPageAdImage(List<String> list, AdMonitorTaskFeedback feedback) throws DocumentException, IOException {
+        //判断第一张图片是否为空
+        if (!com.alibaba.druid.util.StringUtils.isEmpty(feedback.getPicUrl1())) {
+            insertImage(feedback.getPicUrl1(), Image.ALIGN_CENTER, 760, 600);
+        }
+        //判断第二张图片是否为空
+        if (!com.alibaba.druid.util.StringUtils.isEmpty(feedback.getPicUrl2())) {
+            insertImage(feedback.getPicUrl2(), Image.ALIGN_CENTER, 1280, 600);
+        }
+        //判断第三张图片是否为空
+        if (!com.alibaba.druid.util.StringUtils.isEmpty(feedback.getPicUrl3())) {
+            insertImage(feedback.getPicUrl3(), Image.ALIGN_CENTER, 760, 200);
+        }
+        //判断第四张图片是否为空
+        if (!StringUtils.isEmpty(feedback.getPicUrl4())) {
+            insertImage(feedback.getPicUrl4(), Image.ALIGN_CENTER, 1280, 200);
+        }
+
+        //插入地图坐标图
+        Image image6 = Image.getInstance(file_upload_path + "/" + list.get(13) + list.get(14) + ".jpg");
+        image6.setAlignment(Image.ALIGN_CENTER);
+        //控制图片大小
+        image6.scaleAbsolute(540, 300);
+        //控制图片位置
+        image6.setAbsolutePosition(130, 200);
+        document.add(image6);
+    }
+
+    /**
+     * 插入图片
+     *
+     * @param picUrl
+     * @param alignment
+     * @param absoluteX
+     * @param absoluteY
+     * @throws IOException
+     * @throws DocumentException
+     */
+    private void insertImage(String picUrl, int alignment, float absoluteX, float absoluteY) throws IOException, DocumentException {
+        Image image = Image.getInstance(picUrl);
+        float width = image.getWidth();
+        float height = image.getHeight();
+        //合理压缩，height>width，按width压缩，否则按width压缩
+        float percent = getPercent(height, width);
+        image.setAlignment(alignment);
+        //依照比例缩放
+        image.scalePercent(percent);
+        //控制图片大小
+        image.scaleAbsolute(width * percent, percent * height);
+        //控制图片位置
+        image.setAbsolutePosition(absoluteX, absoluteY);
+        document.add(image);
+    }
+
+    /**
+     * 插入logo
+     *
+     * @param adapp
+     * @throws IOException
+     * @throws DocumentException
+     */
+    private void insertLogo(AdApp adapp) throws IOException, DocumentException {
+        //插入Group Logo
+        Image groupLogo = Image.getInstance(real_path + GROUP_LOGO_PATH);
+        groupLogo.setAlignment(Image.ALIGN_CENTER);
+        //控制图片大小和位置
+        groupLogo.scaleAbsolute(140, 50);
+        groupLogo.setAbsolutePosition(1620, 80);
+        document.add(groupLogo);
+        //插入九凤logo
+        Image jfLogo = Image.getInstance(real_path + JF_LOGO_PATH);
+        jfLogo.setAlignment(Image.ALIGN_CENTER);
+        //控制图片大小和位置
+        jfLogo.scaleAbsolute(200, 50);
+        jfLogo.setAbsolutePosition(120, 80);
+        document.add(jfLogo);
+        //插入广告logo
+        Image adLogo = Image.getInstance(adapp.getAppPictureUrl());
+        adLogo.setAlignment(Image.ALIGN_CENTER);
+        //控制图片大小
+        adLogo.scaleAbsolute(200, 60);
+        //控制图片位置
+        adLogo.setAbsolutePosition(1650, 950);
+        document.add(adLogo);
+    }
+
+
+    private float getPercent(float h, float w) {
+        float p = 0.0f;
+        if (h > w) {
+            p = 330 / h;
+        } else {
+            p = 430 / w;
+        }
+        return p;
+    }
+
+}
